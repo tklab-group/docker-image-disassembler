c0d582dab0d1e0c90baa640b780c4b8257820330
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2021-12-14 11:47:28 +0900

    Add NewFileInfoFromTarHeader.

--- disassenmbler/filetree/file_info.go
+++ disassenmbler/filetree/file_info.go
@@ -1,9 +1,14 @@
 package filetree
 
-import "os"
+import (
+	"archive/tar"
+	"io"
+	"os"
+	"path/filepath"
+)
 
 type FileInfo struct {
-	Path     string
+	Name     string
 	TypeFlag byte
 	Linkname string
 	Data     []byte // Data contains actual data in the file.
@@ -12,4 +17,24 @@ type FileInfo struct {
 	Uid      int
 	Gid      int
 	IsDir    bool
+}
+
+// NewFileInfoFromTarHeader extracts the metadata from a tar header and file contents.
+func NewFileInfoFromTarHeader(reader *tar.Reader, header *tar.Header) (*FileInfo, error) {
+	data, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FileInfo{
+		Name:     filepath.Base(header.Name),
+		TypeFlag: header.Typeflag,
+		Linkname: header.Linkname,
+		Data:     data,
+		Size:     header.FileInfo().Size(),
+		Mode:     header.FileInfo().Mode(),
+		Uid:      header.Uid,
+		Gid:      header.Gid,
+		IsDir:    header.FileInfo().IsDir(),
+	}, nil
 }
