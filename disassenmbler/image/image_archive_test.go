88d9025b35835e8e8d0684e00b2e4036e0f358eb
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2022-01-07 10:40:05 +0900

    Add a test to check collecting whiteout files.

--- disassenmbler/image/image_archive_test.go
+++ disassenmbler/image/image_archive_test.go
@@ -1,6 +1,7 @@
 package image
 
 import (
+	"fmt"
 	"github.com/sebdah/goldie/v2"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -37,4 +38,19 @@ func TestImageArchive_GetLatestFileNode(t *testing.T) {
 
 	assert.NotNil(t, imageArchive.GetLatestFileNode("/a/bb/ccc/dddd/eeeee"))
 	assert.Nil(t, imageArchive.GetLatestFileNode("/a/bb/ccc/dddd/eeeee/f"))
+}
+
+func TestCollectWhiteoutFiles(t *testing.T) {
+	imageTarName, _ := testutil.CreateTarImageFromDockerfile(t, "testdata/Dockerfile.whiteout-file")
+	buf := testutil.ReadFileForBuffer(t, imageTarName)
+	imageArchive, err := NewImageArchive(buf)
+	require.NoError(t, err)
+
+	g := goldie.New(t, goldie.WithFixtureDir("testdata/golden"))
+	for i := 0; i < len(imageArchive.Manifest.LayerTarPaths); i++ {
+		fileTree, err := imageArchive.GetFileTreeByLayerIndex(i)
+		require.NoError(t, err)
+
+		g.AssertJson(t, fmt.Sprintf("whiteout-files-layer%d", i), fileTree.WhiteoutFiles)
+	}
 }
