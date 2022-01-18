aed3a859bd0b4a2030ee87de495b36cdeaa1d7b5
oribe1115 <50443000+oribe1115@users.noreply.github.com>
2022-01-18 23:52:24 +0900

    Implemented lsexec.

---
+++ measure/lsexec/ls_exec.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+)
+
+var targetDir string
+
+func init() {
+	flag.StringVar(&targetDir, "targetDir", "/", "target directory to exec ls in the container")
+}
+
+func main() {
+	flag.Parse()
+	args := flag.Args()
+
+	err := run(args, os.Stdout)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+}
+
+func run(args []string, out io.Writer) error {
+	if len(args) != 1 {
+		return fmt.Errorf("1 arg is required")
+	}
+
+	imageID := args[0]
+
+	shellScript := fmt.Sprintf(`#!/bin/sh
+docker run --rm %s /bin/sh -c "find %s -type f -ls" | awk '{ print $11, $7 }'`, imageID, targetDir)
+
+	file, err := os.CreateTemp("/tmp", "*.sh")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(file.Name())
+
+	_, err = file.WriteString(shellScript)
+	if err != nil {
+		return err
+	}
+
+	cmd := exec.Command("sh", file.Name())
+	cmd.Stdout = out
+
+	err = cmd.Run()
+	if err != nil {
+		return fmt.Errorf("faild to exec command: %w", err)
+	}
+
+	return nil
+}
